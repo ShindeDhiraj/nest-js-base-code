d6b2bbb9627ab4acb5ae6a71bce9d77119f24063
Dhiraj Shinde <dhirajs@valueaddsofttech.com>
2025-04-09 21:17:36 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: 'http://localhost:5173', // Frontend URL
+    credentials: true, // Enable credentials (cookies)
+  });
+  app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('Classes')
+    .setDescription('The Tuition classes API description')
+    .setVersion('1.0')
+    .addTag('classes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+
+  // Starts listening for shutdown hooks
+  //  app.enableShutdownHooks();
+
+  await app.listen(4000);
 }
 bootstrap();
